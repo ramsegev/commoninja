8e45c9680dd76cc58c6a2d1202dfb5b29db78dc0
Ram Segev <ramsegev@gmail.com>
2024-06-23 16:39:08 +0300

    Update route.ts
    secure token

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -4,7 +4,7 @@ export async function POST(request: Request) {
     const { username, password } = await request.json();
 
     if (username === 'admin' && password === 'admin') {
-        return NextResponse.json({ token: 'af71ae8a7d20f61b2f93e1385665a6587714f842861259bc' });
+        return NextResponse.json({ token: process.env.TOKEN });
     }
     return NextResponse.json({ error: 'Invalid credentials' }, { status: 401 });
 }
